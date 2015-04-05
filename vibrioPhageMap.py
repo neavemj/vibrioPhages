0e77f6ffd720d6444e3ed7c25ab79d44491555e1
neavemj <matthewjneave1@gmail.com>
2015-04-05 13:33:28 +0300

    ensured correct overlap

--- vibrioPhageMap.py
+++ vibrioPhageMap.py
@@ -4,31 +4,50 @@
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.Graphics import GenomeDiagram
 from reportlab.lib.units import cm
-
+import os
 
 genomeDiag = GenomeDiagram.Diagram("vibrioPhages")
-genomeTrack = genomeDiag.new_track(1, greytrack=False)
-genomeSet = genomeTrack.new_set()
 
 
-BAA450_VCY = open("/Users/neavemj/otherProjects/vibrioCoral/4.17.2.15/PHAST_phage_finder/phageRegions/BAA450_VCY_region.fa")
+phage_region_dir = '/Users/neavemj/otherProjects/vibrioCoral/4.17.2.15/PHAST_phage_finder/phageRegions/'
 
-for line in BAA450_VCY:
-    line = line.strip()
-    cols = line.split()
-    region = cols[0]
-    name = cols[1]
-    region = region.split("..")
-    startRegion = int(region[0])
-    stopRegion = int(region[1])
+for phage_regions in os.listdir(phage_region_dir):
 
-    feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=+1)
-    genomeSet.add_feature(feature, name=name, label=True, sigil="ARROW")
+    phage_handle = open(phage_region_dir + phage_regions)
+    genomeTrack = genomeDiag.new_track(1, name=phage_regions, greytrack=False)
+    genomeSet = genomeTrack.new_set()
+    lineCount = 0
+
+    for line in phage_handle:
+
+        lineCount += 1
+        line = line.strip()
+        cols = line.split()
+        region = cols[0]
+        name = cols[1]
+        if region.startswith("complement"):
+            region = region.lstrip("complement(").rstrip(")").split("..")
+            if lineCount == 1:
+                initPosition = int(region[0])
+            startRegion = int(region[0]) - initPosition
+            stopRegion = int(region[1]) - initPosition
+            feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=-1)
+
+        else:
+            region = region.split("..")
+            if lineCount == 1:
+                initPosition = int(region[0])
+            startRegion = int(region[0]) - initPosition
+            stopRegion = int(region[1]) - initPosition
+            feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=+1)
+
+        genomeSet.add_feature(feature, name=name, label=True, sigil="ARROW")
 
 
-genomeDiag.draw(format='linear', pagesize=(15*cm, 4*cm), fragments=1, start=4431042, end=4438916)
+
+genomeDiag.draw(format='linear', pagesize=("A4"), fragments=1, start=0, end=10000)
 genomeDiag.write("BAA450_VCY.pdf", "pdf")
-
+genomeDiag.write("BAA450_VCY.svg", "svg")
 
 
 
