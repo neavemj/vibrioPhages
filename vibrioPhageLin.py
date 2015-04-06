b6e3c1d380d52b23319784a9d85cce2c95e2f61f
neavemj <matthewjneave1@gmail.com>
2015-04-06 12:54:55 +0300

    added arbitrary tracks for re-use

--- vibrioPhageLin.py
+++ vibrioPhageLin.py
@@ -1,6 +1,7 @@
 
 
 # create linear vibrio phage map using biopython
+# Matthew J. Neave 2.4.2015
 # imported required modules
 
 from Bio.SeqFeature import SeqFeature, FeatureLocation
@@ -8,7 +9,6 @@ from Bio.Graphics import GenomeDiagram
 from Bio.Graphics.GenomeDiagram import CrossLink
 from reportlab.lib import colors
 from reportlab.lib.units import cm
-import os
 
 
 # first read through PHAST results and extract information
@@ -19,9 +19,11 @@ phage_regions_file = '/Users/neavemj/otherProjects/vibrioCoral/4.17.2.15/PHAST_p
 
 max_len = 0
 phageDict = {}
+phageList = []
 
 for phage_regions in open(phage_regions_file):
     phage_regions = phage_regions.strip()
+    phageList.append(phage_regions)
     print "processing file:", phage_regions
     phage_handle = open(phage_regions_dir + phage_regions)
     phageDict[phage_regions] = {}
@@ -55,14 +57,76 @@ for phage_regions in open(phage_regions_file):
 
 print phageDict
 
+
 # ok create the genome diagram and tracks for each phage
 
 genomeDiag = GenomeDiagram.Diagram("vibrioPhages")
-genomeTrack = genomeDiag.new_track(1, name=phage_regions, greytrack=True, start=0, end=total_len)
-genomeSet = genomeTrack.new_set()
+
+for genome in phageList:
+    genomeTrack = genomeDiag.new_track(1, name=genome, greytrack=True, start=0, end=phageDict[genome]["totalLength"])
 
 
-feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=-1)
-feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=+1)
+# dictionary to translate phage name into track number
 
-genomeSet.add_feature(feature, name=name, label=False, label_position="start", sigil="ARROW")
+phageTrack = {"BAA450_VCY_region.fa" : 9, "P1_4_VCY.fa" : 8, "RE98_web_1_kappa.fa" : 7, "OCN008_K139_region.fa" : 6, "P1_1_KS14.fa" : 5, "P1_3_Yersin.fa" : 4, "RE98_web_2_ep3.fa" : 3, "P1_2_CTX.fa" : 2, "OCN014_VP882_region.fa" : 1}
+
+
+# read in all-vs-all blast results to create links between similar genes
+# file should be first gene name, second gene name then percent similarity
+# want to add these links first so that the arrows go over the top
+
+links_file_handle = open("/Users/neavemj/otherProjects/vibrioCoral/4.17.2.15/PHAST_phage_finder/3.allVsall/links.txt")
+links_dir = '/Users/neavemj/otherProjects/vibrioCoral/4.17.2.15/PHAST_phage_finder/3.allVsall/'
+
+
+for link_tracks in open(links_file_handle):
+    link_tracks = link_tracks.strip()
+    links_handle = open(links_dir + link_tracks)
+
+
+    track_1 = genomeDiag.tracks[phageTrack[link_tracks]]
+
+    for connection in links_handle:
+        connection = connection.strip()
+        values = connection.split("\t")
+        track_1_start = float(values[0])
+        track_1_end = float(values[1])
+        track_2_start = float(values[2])
+        track_2_end = float(values[3])
+        score = float(values[4])
+
+
+
+track_BAA450 = genomeDiag.tracks[9]
+track_P1_4 = genomeDiag.tracks[8]
+
+for connection in BAAvsP1_4_handle:
+    connection = connection.strip()
+    values = connection.split("\t")
+    track_BAA450_start = float(values[0])
+    track_BAA450_end = float(values[1])
+    track_P1_4_start = float(values[2])
+    track_P1_4_end = float(values[3])
+    score = float(values[4])
+
+    color = colors.linearlyInterpolatedColor(colors.white, colors.firebrick, 0, 100, score)
+    link_xy = CrossLink((track_BAA450, track_BAA450_start, track_BAA450_end), (track_P1_4, track_P1_4_start, track_P1_4_end), color=color)
+
+    feature = SeqFeature(FeatureLocation(int(track_BAA450_start), int(track_BAA450_end), strand=0))
+    genomeSet = track_BAA450.new_set()
+    genomeSet.add_feature(feature, label=False, label_position="start", sigil="BOX", color=color)
+
+
+    genomeDiag.cross_track_links.append(link_xy)
+
+
+
+# feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=-1)
+# feature = SeqFeature(FeatureLocation(startRegion, stopRegion), strand=+1)
+#
+# genomeSet = genomeTrack.new_set()
+# genomeSet.add_feature(feature, name=name, label=False, label_position="start", sigil="ARROW")
+#
+# genomeDiag.draw(format='linear', pagesize=("A4"), fragments=1, start=0, end=max_len)
+# genomeDiag.write("phageLinear.pdf", "pdf")
+# genomeDiag.write("phageLinear.svg", "svg")
